d60c2ccbeafc059573a8f36e1437087cd4612d73
a7611818 <869329190@qq.com>
2018-12-30 23:36:03 +0800

    new test

--- src/main/java/com/sz/util/GitTest.java
+++
@@ -1,6 +0,0 @@
-package com.sz.util;
-
-public class GitTest {
-    //123123123123123123123 s
-    private Integer hahssasd123asda;
-}
--- src/main/java/com/sz/util/Test2.java
+++
@@ -1,7 +0,0 @@
-package com.sz.util;
-
-public class Test2 {
-    public static void main(String[] args) {
-
-    }
-}
